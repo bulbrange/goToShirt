68caae469ca505959469e7206d0d3d54911b33e5
Andrés Herrero Fragoso <andresherrerof@gmail.com>
2018-10-18 20:16:17 +0200

    Full modular Logging screen

--- client/src/screens/Logging.js
+++ client/src/screens/Logging.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Image, View } from 'react-native';
 import Grid from '../styles/grid';
 import LogginPanel from './components/LogginPanel';
+import MainHeader from './components/MainHeader';
 
 class Logging extends Component {
   constructor(props) {
@@ -43,14 +44,7 @@ class Logging extends Component {
     ];
     return (
       <View style={Grid.grid}>
-        <View style={[Grid.row, Grid.p0, { flex: 0.4 }]}>
-          <Image
-            style={{ flex: 1, width: null, height: null }}
-            source={{
-              uri: 'https://dz2cdn4.dzone.com/storage/article-thumb/210027-thumb.jpg',
-            }}
-          />
-        </View>
+        <MainHeader />
         <LogginPanel attrs={textInputAttrs} />
       </View>
     );
---
+++ client/src/screens/components/MainHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import Grid from '../../styles/grid';
+
+const MainHeader = () => (
+  <View style={[Grid.row, Grid.p0, { flex: 0.4 }]}>
+    <Image
+      style={{ flex: 1, width: null, height: null }}
+      source={{
+        uri: 'https://dz2cdn4.dzone.com/storage/article-thumb/210027-thumb.jpg',
+      }}
+    />
+  </View>
+);
+
+export default MainHeader;
