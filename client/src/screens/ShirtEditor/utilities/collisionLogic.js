a9c0bd4e858939977699d7a603d667d0e850d757
Andrés Herrero Fragoso <andresherrerof@gmail.com>
2018-11-07 20:44:00 +0100

    Formula to collision is not working properly, coby is needed

--- client/src/screens/ShirtEditor/utilities/collisionLogic.js
+++ client/src/screens/ShirtEditor/utilities/collisionLogic.js
@@ -1,15 +1,26 @@
 const offset = (renderSize, width, height) => {
-  const maxXX = width - renderSize * 2; // render 80 ---> 200
+  // const maxXX = (width * (renderSize / 1.75)) / renderSize; // MIN OK --> 80
+  const maxXX = (width * (renderSize / 2.15)) / renderSize; // MIN OK --> 110
   console.log('WIDTH @ OFFSET: ', width);
   console.log('RENDERSIZE: ', renderSize);
   console.log('MAXX: ', maxXX);
-  if (renderSize === 80) return { x: { maxX: maxXX, minX: 72 }, y: { maxY: 310, minY: 70 } };
-  if (renderSize === 110) return { x: { maxX: 165, minX: 66 }, y: { maxY: 260, minY: 70 } };
+  if (renderSize === 80) {
+    return {
+      x: { maxX: (width * (renderSize / 1.75)) / renderSize, minX: 72 },
+      y: { maxY: 310, minY: 70 },
+    };
+  }
+  if (renderSize === 110) {
+    return {
+      x: { maxX: (width * (renderSize / 2.15)) / renderSize, minX: 66 },
+      y: { maxY: 260, minY: 70 },
+    };
+  }
   return { x: { maxX: 88, minX: 65 }, y: { maxY: 180, minY: 70 } };
 };
 
 const collisionX = (posX, renderSize, width, height) => {
-  const { maxX, minX } = offset(renderSize, width, height).x;
+  const { maxX, minX } = offset(renderSize, width, 0).x;
 
   if (posX > maxX) return maxX;
   if (posX < minX) return minX;
@@ -17,7 +28,7 @@ const collisionX = (posX, renderSize, width, height) => {
 };
 
 const collisionY = (posY, renderSize, width, height) => {
-  const { maxY, minY } = offset(renderSize, width, height).y;
+  const { maxY, minY } = offset(renderSize).y;
 
   if (posY > maxY) return maxY;
   if (posY < minY) return minY;
