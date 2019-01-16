e3df0da0db7e34344de02fe527ac7c5db006f708
JvMolli <jaimemg@outlook.com>
2019-01-16 09:09:44 +0100

    zzzz

--- client/src/ip.js
+++ client/src/ip.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export default (IP = '172.16.101.22');
-=======
-export default (IP = '192.168.1.43');
->>>>>>> 7ed97d1cae35062a0e9e4397cab064ce7a1d9c5a
 // 192.168.1.133
--- client/src/screens/Dashboard/container/Dashboard.js
+++ client/src/screens/Dashboard/container/Dashboard.js
@@ -3,16 +3,25 @@ import { graphql, compose } from 'react-apollo';
 import { TSHIRTS } from '../../../queries/tshirt.queries';
 import { withLoading } from '../../../components/withLoading';
 import Dashboard from '../components/Dashboard';
+import USER_BY_ID from '../../../queries/userById.query';
 
 const pepetter = graphql(TSHIRTS, {
-  options: () => ({ variables: { userId: 1 } }), // fake for now
+  options: () => ({ variables: { userId: 21 } }), // fake for now
   props: ({ data: { loading, tshirts } }) => ({
     loading,
     tshirts,
   }),
 });
+const userByIdQuery = graphql(USER_BY_ID, {
+  options: () => ({ variables: { id: 21 } }), // fake for now
+  props: ({ data: { loading, userById } }) => ({
+    loading,
+    userById,
+  }),
+});
 
 export default compose(
   pepetter,
+  userByIdQuery,
   withLoading,
 )(Dashboard);
