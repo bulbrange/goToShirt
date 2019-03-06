0211e86d010e199565d0fbfe55bf1ebb90125bc3
dani <pangeanoteniafrontera@gmail.com>
2019-03-06 19:38:57 +0100

    added textures default

--- client/src/screens/ShirtEditor/components/OutputPanel/components/mockedImg.js
+++ client/src/screens/ShirtEditor/components/OutputPanel/components/mockedImg.js
@@ -1,13 +1,33 @@
 import IP from '../../../../../ip';
 
-const img1 = `http://${IP}:8888/textures/bansky9454464564651616.png`;
+
 
 
 const mockedImages = [
   {
     id: 1,
     name: '',
-    source: img1,
+    source: `http://${IP}:8888/textures/bansky9454464564651616.png`,
+  },
+  {
+    id: 2,
+    name: '',
+    source: `http://${IP}:8888/textures/chewaka.png`,
+  },
+  {
+    id: 3,
+    name: '',
+    source: `http://${IP}:8888/textures/it.png`,
+  },
+  {
+    id: 4,
+    name: '',
+    source: `http://${IP}:8888/textures/rebel.png`,
+  },
+  {
+    id: 5,
+    name: '',
+    source: `http://${IP}:8888/textures/surtich.png`,
   },
 
 ];
